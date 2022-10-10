561cfc891ab29eaada8f99b5c78a954e8f6fc123
Aspyn Call <abscall01@gmail.com>
2022-10-10 14:31:58 -0400

    Added picture, changed background, added red rectangles, and added unordered list

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,43 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">Aspyn Call</header>
+            <h1>Welcome to my React site!</h1>
             <p>COS420</p>
             <p>Hello World</p>
             <Button
-                onClick={() => console.log("I am logged")}
+                onClick={() => console.log("Hello World!")}
                 aria-label="Log Hello World"
             >
-                Click Me!
+                Log Hello World
             </Button>
             <Container>
                 <Row>
-                    <Col>First column</Col>
                     <Col>
+                        <div
+                            style={{
+                                width: 20,
+                                height: 30,
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                        Grocery List
+                        <ul>
+                            <li>Eggs</li>
+                            <li>Rice</li>
+                            <li>Chicken</li>
+                            <li>Shrimp</li>
+                            <li>Steak</li>
+                            <li>Mixed Veggies</li>
+                        </ul>
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                width: 20,
+                                height: 30,
+                                backgroundColor: "red"
+                            }}
+                        ></div>
                         <img
                             src="../assets/uzui_and_wives.jpg"
                             alt="Tengen Uzui and wives lol"
