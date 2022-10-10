724f7940568c51f9fccf3b15b2df404869dab170
Aspyn Call <abscall01@gmail.com>
2022-10-10 13:50:23 -0400

    Added button, columns, and image

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
@@ -7,6 +8,23 @@ function App(): JSX.Element {
             <header className="App-header">Aspyn Call</header>
             <p>COS420</p>
             <p>Hello World</p>
+            <Button
+                onClick={() => console.log("I am logged")}
+                aria-label="Log Hello World"
+            >
+                Click Me!
+            </Button>
+            <Container>
+                <Row>
+                    <Col>First column</Col>
+                    <Col>
+                        <img
+                            src="../assets/uzui_and_wives.jpg"
+                            alt="Tengen Uzui and wives lol"
+                        />
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
