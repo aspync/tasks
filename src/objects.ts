c00b8dd1cfec60087dd7b8f57ed15fc72938eba1
Aspyn Call <abscall01@gmail.com>
2022-10-26 15:46:29 -0400

    Completed addOption function

--- src/objects.ts
+++ src/objects.ts
@@ -173,7 +173,12 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-    return question;
+    const newQuestion = {
+        ...question,
+        options: [...question.options, newOption]
+    };
+    console.log(newQuestion);
+    return newQuestion;
 }
 
 /**
