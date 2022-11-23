95cd97520775fd12c2491dc968428b83c3e035a3
Aspyn Call <abscall01@gmail.com>
2022-11-23 13:32:01 -0500

    Fixed DoubleHalf component

--- src/App.tsx
+++ src/App.tsx
@@ -14,15 +14,20 @@ import { ChooseTeam } from "./bad-components/ChooseTeam";
 
 function App(): JSX.Element {
     return (
-        <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-            </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
-        </div>
+        <>
+            <div className="App">
+                <header className="App-header">
+                    UD CISC275 with React Hooks and TypeScript
+                </header>
+                <p>
+                    Edit <code>src/App.tsx</code> and save. This page will
+                    automatically reload.
+                </p>
+            </div>
+            <div>
+                <DoubleHalf></DoubleHalf>
+            </div>
+        </>
     );
 }
 
--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,24 +1,32 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { dhValue, setDhValue } from "./DoubleHalfState";
 
-function Doubler(): JSX.Element {
-    return <Button onClick={() => setDhValue(2 * dhValue)}>Double</Button>;
+interface SetValueProps {
+    setValue: (newValue: number) => void;
+    value: number;
 }
 
-function Halver(): JSX.Element {
-    return <Button onClick={() => setDhValue(0.5 * dhValue)}>Halve</Button>;
+function Doubler({ setValue, value }: SetValueProps): JSX.Element {
+    return <Button onClick={() => setValue(2 * value)}>Double</Button>;
+}
+
+function Halver({ setValue, value }: SetValueProps): JSX.Element {
+    return <Button onClick={() => setValue(0.5 * value)}>Halve</Button>;
 }
 
 export function DoubleHalf(): JSX.Element {
+    const [dhValue, setDhValue] = useState<number>(10);
+
     return (
         <div>
             <h3>Double Half</h3>
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Doubler></Doubler>
-            <Halver></Halver>
+            <Doubler setValue={setDhValue} value={dhValue}></Doubler>
+            <Halver setValue={setDhValue} value={dhValue}></Halver>
         </div>
     );
 }
+
+export default DoubleHalf;
